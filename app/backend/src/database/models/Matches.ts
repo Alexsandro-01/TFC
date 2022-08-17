302bb44019a1a228fd039f47613ea24c824ccac8
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-08-17 18:03:28 -0300

    feat: criação das models do Sequelize para as tabelas do BD

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,65 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+
+class Matches extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team',
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team',
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team_gols',
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+Teams.belongsTo(Matches, { foreignKey: 'homeTeam', as: 'team' });
+Teams.belongsTo(Matches, { foreignKey: 'awayTeam', as: 'team' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Matches;
---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,30 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  public id!: number;
+  public teamName!: string;
+}
+
+Teams.init(
+  {
+    id: {
+      type: INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    teamName: {
+      type: STRING,
+      allowNull: false,
+      field: 'team_name',
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+  },
+);
+
+export default Teams;
