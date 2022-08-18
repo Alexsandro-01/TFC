9f5290061dcd9896e8298f41420841727bec2607
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-08-17 23:11:45 -0300

    feat: criação do Service para lidar com Requisições de login de usuário

---
+++ app/backend/src/services/UserService.ts
@@ -0,0 +1,30 @@
+import UserModel from '../database/models/Users';
+import { IUserService, Login } from '../interface/IUser';
+import Validations from './Validations';
+import Token from './Token';
+
+class UserService implements IUserService<UserModel> {
+  login = async (data: Login): Promise<string> => {
+    const user: UserModel | null = await UserModel.findOne(
+      {
+        raw: true, where: { email: data.email },
+      },
+    );
+
+    await Validations.ValidLogin(user as UserModel, data.password);
+
+    const token = Token.makeToken(user as UserModel);
+    return token;
+  };
+}
+
+export default UserService;
+
+// const a = new UserService();
+// const b = a.login({ email: 'admin@admin.com', password: 'secret_admin' });
+// console.log(b);
+// const s = Cript.decript(
+//   'secret_admin',
+//   '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+// );
+// console.log(s);
