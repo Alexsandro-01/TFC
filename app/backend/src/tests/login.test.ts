c09e7b6d3af0f501b6b5425cc00e3856f7178888
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-08-18 17:42:17 -0300

    chore: adição de testes para a falha na rota /login
    Trata casos de dados faltantes na requisição de login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -18,25 +18,50 @@ import {
  } from './mocks';
 
 describe('Comportamento da rota de /login', () => {
-  beforeEach(() => {
-    sinon.stub(Users, 'findOne').resolves(validReturnUser as Users);
-    sinon.stub(jwt, 'sign').resolves(token)
+
+  describe('Succes cases', () => {
+    beforeEach(() => {
+      sinon.stub(Users, 'findOne').resolves(validReturnUser as Users);
+      sinon.stub(jwt, 'sign').resolves(token)
+    });
+  
+    afterEach(sinon.restore);
+  
+    it('Shoul return status 200 if valid user', async () => {
+      const response: Response = await chai.request(app)
+        .post('/login').send(validLogin);
+  
+      chai.expect(response.status).to.equal(200);
+    });
+  
+    it('Shoul return a token if valid user', async () => {
+      const response: Response = await chai.request(app)
+        .post('/login').send(validLogin);
+      
+      const body = response.body;
+      chai.expect(body.token).to.equal(token);
+    });
   });
 
-  afterEach(sinon.restore);
+  describe('Failure cases', () => {
 
-  it('Shoul return status 200 if valid user', async () => {
-    const response: Response = await chai.request(app)
-      .post('/login').send(validLogin);
-
-    chai.expect(response.status).to.equal(200);
+    it('Shoul return a message "All fields must be filled" and status 400 if email is\'nt provaided', async () => {
+      const response: Response = await chai.request(app)
+        .post('/login').send({ email: validLogin.email });
+        
+      const body = response.body;
+      const erroMessage = "All fields must be filled"
+      chai.expect(body.message).to.equal(erroMessage);
+    });
+  
+    it('Shoul return a message "All fields must be filled" and status 400 if email is\'nt provaided', async () => {
+      const response: Response = await chai.request(app)
+        .post('/login').send({ password: validLogin.password });
+      
+      const body = response.body;
+      const erroMessage = "All fields must be filled"
+      chai.expect(body.message).to.equal(erroMessage);
+    });
+  
   });
-
-  it('Shoul return a token if valid user', async () => {
-    const response: Response = await chai.request(app)
-      .post('/login').send(validLogin);
-
-    chai.expect(response.body.token).to.equal(token);
-  });
- 
 })
